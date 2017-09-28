73454e3c95bb6f88a5d477d27589068746bceec0
Яворский Владимир <vovka.krevedko.su>
2017-09-28 18:01:33 +0300

    убран дебаг

--- main.c
+++ main.c
@@ -17,7 +17,7 @@
 
 /* 
  * File:   main.c
- * Author: vovka
+ * Author: Vladimir Yavorskiy <vovka@krevedko.su>
  *
  * Created on 28 сентября 2017 г., 10:53
  */
@@ -43,6 +43,7 @@
 #define GCODE_GROUP_M 2
 
 int state = STATE_FRAME;
+char *gcode_symbol;
 char *digits;
 char *digit;
 double x;
@@ -50,16 +51,16 @@ double y;
 double z;
 double feed;
 
-int readDigits(char *symbol) {
-    if (*symbol >= '0' && *symbol <= '9' || *symbol == '.') {
-        *digit = *symbol;
+int readDigits() {
+    if (*gcode_symbol >= '0' && *gcode_symbol <= '9' || *gcode_symbol == '.') {
+        *digit = *gcode_symbol;
         digit++;
         return 0;
     } else {
         *digit = 0;
         digit = digits;
-        if (*symbol == '\n') {
-            symbol--;
+        if (*gcode_symbol == '\n') {
+            gcode_symbol--;
         }
         return 1;
     }
@@ -71,7 +72,7 @@ int readDigits(char *symbol) {
 int main(int argc, char** argv) {
 
     printf("G-CODE файл: %s\n", argv[1]);
-    char *gcode_symbol = argv[1];
+    gcode_symbol = argv[1];
     digits = malloc(10);
     digit = digits;
     while (state != STATE_END) {
@@ -87,7 +88,6 @@ int main(int argc, char** argv) {
                 }
                 break;
             case STATE_COMMAND_GROUP:
-                printf("STATE_COMMAND_GROUP: %c\n", *gcode_symbol);
                 switch (*gcode_symbol) {
                     case 'G':
                         state = STATE_COMMAND_GROUP_G;
@@ -95,10 +95,8 @@ int main(int argc, char** argv) {
                 }
                 break;
             case STATE_COMMAND_GROUP_G:
-                printf("STATE_COMMAND_GROUP_G: %c\n", *gcode_symbol);
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     int gcode_number = atoi(digits);
-                    printf("STATE_COMMAND_GROUP_G: %d\n", gcode_number);
                     switch (gcode_number) {
                         case 0:
                             state = STATE_G00_RAPID;
@@ -125,21 +123,21 @@ int main(int argc, char** argv) {
                 }
                 break;
             case STATE_G00_RAPID_X:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     x = atof(digits);
                     printf("STATE_G00_LINEAR_X: %f\n", x);
                     state = STATE_G00_RAPID;
                 }
                 break;
             case STATE_G00_RAPID_Y:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     y = atof(digits);
                     printf("STATE_G00_LINEAR_Y: %f\n", y);
                     state = STATE_G00_RAPID;
                 }
                 break;
             case STATE_G00_RAPID_Z:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     z = atof(digits);
                     printf("STATE_G00_LINEAR_Z: %f\n", z);
                     state = STATE_G00_RAPID;
@@ -165,28 +163,28 @@ int main(int argc, char** argv) {
                 break;
 
             case STATE_G01_LINEAR_X:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     x = atof(digits);
                     printf("STATE_G01_LINEAR_X: %f\n", x);
                     state = STATE_G01_LINEAR;
                 }
                 break;
             case STATE_G01_LINEAR_Y:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     y = atof(digits);
                     printf("STATE_G01_LINEAR_Y: %f\n", y);
                     state = STATE_G01_LINEAR;
                 }
                 break;
             case STATE_G01_LINEAR_Z:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     z = atof(digits);
                     printf("STATE_G01_LINEAR_Z: %f\n", z);
                     state = STATE_G01_LINEAR;
                 }
                 break;
             case STATE_G01_LINEAR_FEED:
-                if (readDigits(gcode_symbol) == 1) {
+                if (readDigits() == 1) {
                     feed = atof(digits);
                     printf("STATE_G01_LINEAR_FEED: %f\n", feed);
                     state = STATE_G01_LINEAR;
